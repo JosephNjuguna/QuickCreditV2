ccd8a007020fc11af9eea1f29bc64edf83277d85
JosephNjuguna <josephnjuguna482@gmail.com>
2019-04-22 11:39:06 +0300

    [ft #165484553] display user login modal

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,20 +1,36 @@
 // div
 var signupmodal = document.getElementById('signupModal');
+var loginmodal = document.getElementById('loginModal');
 // button
 var signupbtn  = document.getElementById("signupBtn");
+var loginbtn  = document.getElementById("loginBtn");
+var navloginbtn  = document.getElementById("navloginBtn");
 // modal close span
-var closesignupSpan = document.getElementsByClassName("close")[0];
+var closesignupSpan = document.getElementById("closeSignup");
+var closeloginSpan = document.getElementById("closeLogin");
 
 signupbtn.onclick = function() {
     signupmodal.style.display = "flex";  
 };
-
 closesignupSpan.onclick = function() {
     signupmodal.style.display = "none";  
 };
-
+// user login
+loginbtn.onclick = function() {
+    loginmodal.style.display = "flex";  
+};
+closeloginSpan.onclick = function() {
+    loginmodal.style.display = "none";  
+};
+// navigation buttons
+navloginbtn.onclick = function() {
+    loginmodal.style.display = "flex";  
+};
+// window events
 window.onclick = function(event) {
     if (event.target == signupmodal) {
         signupmodal.style.display = "none";
+    }else if(event.target == loginmodal){
+        loginmodal.style.display = "none";
     }
 };
