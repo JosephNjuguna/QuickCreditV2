476ee0a9ea69ffcc8ed6e47e0cb68ad6450a84de
JosephNjuguna <josephnjuguna482@gmail.com>
2019-04-23 14:12:29 +0300

    [ft #165484702] use javascript to display div with all repaid loans

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -1,16 +1,26 @@
 // admin dashboard modal
 var applicationLoan = document.getElementById('application');
 var verifyUser = document.getElementById('verification');
+var repaidLoans = document.getElementById('repaid');
 // dashboard buttons
 var newLoans = document.getElementById("newLoans");
 var userVerification = document.getElementById("userVerification");
+var loanRepaid = document.getElementById("repaidLoan");
 
 newLoans.onclick = function () {
     applicationLoan.style.display ="flex";
     verifyUser.style.display = "none";
+    repaidLoans.style.display ="none";
 };
 
 userVerification.onclick = function () {
     applicationLoan.style.display ="none";
     verifyUser.style.display = "flex";
+    repaidLoans.style.display ="none";
+};
+
+loanRepaid.onclick = function () {
+    applicationLoan.style.display ="none";
+    verifyUser.style.display = "none";
+    repaidLoans.style.display ="flex";
 };
