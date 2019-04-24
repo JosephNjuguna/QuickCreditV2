5b7e6c7ddd0a8102868f76a46c0cf7897f71fda7
JosephNjuguna <josephnjuguna482@gmail.com>
2019-04-24 18:32:24 +0300

    [ft #165484642] display modal with  details of specific loan

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -3,11 +3,16 @@ var applicationLoan = document.getElementById('application');
 var verifyUser = document.getElementById('verification');
 var repaidLoans = document.getElementById('repaid');
 var currentLoans = document.getElementById('current');
-// dashboard buttons
+var loandetail = document.getElementById('loandetail');
+// dashboard buttons  
 var newLoans = document.getElementById("newLoans");
 var userVerification = document.getElementById("userVerification");
 var loanRepaid = document.getElementById("repaidLoan");
 var notPaidloan = document.getElementById("notPaidloan");
+var loan_detail = document.getElementById("loan_detail");
+
+// div link
+var span = document.getElementById("loandetailModal")
 
 newLoans.onclick = function () {
     applicationLoan.style.display ="flex";
@@ -35,4 +40,17 @@ notPaidloan.onclick = function () {
     verifyUser.style.display = "none";
     repaidLoans.style.display ="none";
     currentLoans.style.display = "flex";
+};
+
+loan_detail.onclick= function(){
+    loandetail.style.display = "flex";
+};
+span.onclick = function() {    
+    loandetail.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == loandetail) {
+        loandetail.style.display = "none";
+    }
 };
