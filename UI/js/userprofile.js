e58cfbecfa6e30dd5ecabc65e707a731340427fe
JosephNjuguna <josephnjuguna482@gmail.com>
2019-04-25 03:17:28 +0300

    [ch #165570315] use javascript to toggle navbar on small screen

--- UI/js/userprofile.js
+++ UI/js/userprofile.js
@@ -2,27 +2,39 @@
 var userModal = document.getElementById('userdata');
 var payloanModal = document.getElementById('payloan');
 var historyModal = document.getElementById('history');
+var ul = document.getElementsByClassName("nav-ul-right")[0];
+
 // navbar button
 var navProfileBtn = document.getElementById("navProfilebtn");
 var navpayloanBtn = document.getElementById("navpayLoanbtn");
 var navhistoryBtn = document.getElementById("navHistorybtn");
 // close modal button
-var span = document.getElementById("payloanModal")
+var span = document.getElementById("payloanModal");
+// show navbar 
+var shownavBtn = document.getElementById("showNav");
+
+// show navbar
+shownavBtn.onclick = function() {
+    ul.classList.toggle("hidden");
+};
 // navigation buttons userprofile
 navProfileBtn.onclick= function(){
     userModal.style.display = "flex";
     historyModal.style.display ="none";
+    ul.classList.toggle("hidden");
 };
 
 navpayloanBtn.onclick= function(){
     userModal.style.display = "flex";
     payloanModal.style.display = "flex";
     historyModal.style.display ="none"; 
+    ul.classList.toggle("hidden");
 };
 
 navhistoryBtn.onclick = function () {
     userModal.style.display = "none";
     historyModal.style.display ="flex";
+    ul.classList.toggle("hidden");
 };
 
 span.onclick = function() {
