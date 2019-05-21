ea6ad9737b03c0e4e14aca62e786bc852f71832d
JosephNjuguna <josephnjuguna482@gmail.com>
2019-05-21 06:24:15 +0200

    [ft #166108972] add user login route

---
+++ server/routes/users.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import controllers from '../controllers/Users';
+import validate from '../middleware/validations';
+
+const router = express.Router();
+
+router.post('/signup', validate.validatesignup, validate.validatenewEmail, controllers.signup);
+router.post('/login', validate.validatelogin, validate.validateexistingEmail, controllers.login);
+
+export default router;
