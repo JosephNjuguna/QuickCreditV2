a5a371e2974a17aaaeaf27116b290c5cbbca0228
JosephNjuguna <josephnjuguna482@gmail.com>
2019-05-21 06:22:31 +0200

    [ft #166108972] add  routes to app root

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import routes from './server/routes';
 
 require('dotenv').config();
 
@@ -8,5 +9,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true,
 }));
+routes(app);
 
 export default app;
