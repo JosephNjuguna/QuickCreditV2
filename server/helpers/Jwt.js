9790ae03ee4da6e18d08db32e4813488c1c3547d
JosephNjuguna <josephnjuguna482@gmail.com>
2019-05-21 13:54:08 +0200

    [ft #166108972] update Token class

--- server/helpers/Jwt.js
+++ server/helpers/Jwt.js
@@ -4,9 +4,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 class Token {
-  static generateToken(payload) {
+  static generateToken(email, userid, firstname, lastname, address) {
+    const payload = {email, userid, firstname, lastname, address}
     const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: 60 * 60 * 24 * 7 });
     return token;
+  }
+  static checkToken(token, res) {
+    if (!token || token === '') {
+      return reqResponses.handleError(400, 'Token required', res);
+    }
+    return true;
   }
 }
 
