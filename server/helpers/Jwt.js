183f6843505a234e8304a67186924272d38e52b0
JosephNjuguna <josephnjuguna482@gmail.com>
2019-05-21 04:44:48 +0200

    [ft #166108973] add function to generate token

---
+++ server/helpers/Jwt.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class Token {
+  static generateToken(payload) {
+    const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: 60 * 60 * 24 * 7 });
+    return token;
+  }
+}
+
+export default Token;
